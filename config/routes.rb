0e60ab8bf2eb5fad8babd00995625ed5b4d88312
Adrian Sanchez <adrian.saz@gmail.com>
2010-11-07 02:23:33 -0600

    landpage to site rest

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 DemoFlexObservers::Application.routes.draw do
   resources :site_user_infos
-
   resources :users
-
   resources :sites
 
   # The priority is based upon order of creation:
@@ -54,7 +52,7 @@ DemoFlexObservers::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "sites#index"
 
   # See how all your routes lay out with "rake routes"
 
