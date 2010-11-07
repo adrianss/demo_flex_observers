a09a801711599ef2a7c136ed4693a130b25c2f15
Adrian Sanchez <adrian.saz@gmail.com>
2010-11-07 01:29:40 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoFlexObservers::Application.config.secret_token = 'c8c24f7686cf419eec40d9222e6f8f13f895be4069419a0ea6d0eb69db1e55d7899154339dac6aac653b6cfd057729491d7710a283704f6cb711ea5013279f30'
